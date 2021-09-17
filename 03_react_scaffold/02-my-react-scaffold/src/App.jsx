053922e2c9ea1f3e3540b6317b84afa36863a9ed
bnytezz <bnytezz@163.com>
2021-09-17 18:35:03 +0800

    feat: add my react scaffold

---
+++ 03_react_scaffold/02-my-react-scaffold/src/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+/**
+ * 创建组件
+ */
+export default class App extends Component {
+    render() {
+        return (
+            <div>Hello React</div>
+        )
+    }
+} 
